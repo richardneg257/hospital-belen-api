a1d9d6ad272d8d351de8f2cc18302e56c5faf4bf
Richard Negron <richard.negron.257@gmail.com>
2023-04-17 17:07:58 -0500

    estructura inicial

---
+++ src/main/java/com/hospital/belen/BelenApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.belen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/belen/BelenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.belen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
